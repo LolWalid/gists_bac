556f41a36c1a6a14aa7d53b62bed8ada9fa1af90
WalidSeoudi <walid.seoudi@gmail.com>
2015-09-03 19:00:17 +0200

    add bootstrap

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.2.1'
 gem 'mysql2'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
@@ -53,6 +54,7 @@ gem 'haml-rails'
 
 gem 'devise'
 
-gem 'twitter-bootstrap-rails'
+gem 'bootstrap-sass'
+gem 'autoprefixer-rails'
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -22,23 +22,5 @@ module BootstrapFlashHelper
     content_tag(:div, flash_messages.join("\n").html_safe, class: 'info', id: 'js-window-info')
   end
 
-  def message_flash
-    flash_messages = []
-    flash.each do |type, message|
-      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
-      next if message.blank?
-
-      type = type.to_sym
-      type = :success if type.to_s == :notice.to_s
-      type = :danger   if type.to_s == :alert.to_s
-      next unless ALERT_TYPES.include?(type)
-
-      Array(message).each do |msg|
-        text = content_tag(:div, msg.html_safe, :class => "alert fade in alert-#{type}")
-        flash_messages << text if msg
-      end
-    end
-    content_tag(:div, flash_messages.join("\n").html_safe, class: 'info', id: 'js-window-info')
-  end
 end
 
